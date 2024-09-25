85cd844a30380e4e5621368affe42eef2829c260
prasiddhisuke <156493970+prasiddhisuke@users.noreply.github.com>
2024-09-24 23:15:34 -0700

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,12 @@
+dic=('a','b','c','d')
+#print(dic)
+student={'name':'prasiddhi','address':'pune'}
+print(student)
+student['address']=wagholi
+print(student)
+# dic.clear()
+#print(dic)
+#del dic
+print(dic)
+del student['name']
+print(student)
