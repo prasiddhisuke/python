6ba429f8fe96821272324714fd51610907bb9df5
prasiddhisuke <156493970+prasiddhisuke@users.noreply.github.com>
2024-09-27 22:46:16 -0700

    Add files via upload

---
+++ slipe11.py
@@ -0,0 +1,16 @@
+a=(1,2,3)
+b=(3,5,7)
+c=(2,3,1)
+print("oroginal lists:")
+print(a)
+print(b)
+print(c)
+print("\n Element-wise sum of said tuples:")
+result=tuple(map(sum,zip(a,b,c)))
+print(result)
+lst1=[1,5,7]
+lst2=[5,3,7]
+a=(lst1)
+b=(lst2)
+print(a+b)
+                                                                         
